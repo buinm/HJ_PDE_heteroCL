f8511ea184b424a1d3bbe49aedce4562ec4853a4
buiminhb <buiminhb@cs-arch-22.cmpt.sfu.ca>
2020-01-24 17:54:59 -0800

    Completing the algorithm

--- HJ_PDE_HeteroCL.py
+++ HJ_PDE_HeteroCL.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import math
 
+hcl.config.init_dtype = hcl.Float()
 
 class grid:
   def __init__(self, max, min, pts_each_dim, pDim):
@@ -17,9 +18,26 @@ class grid:
 # Hardcoded these information
 
 # Global variables
+def cleaner_HJ_PDE_solver(V_new, V_init, thetas, dx):
+
+    # Calculate dV_dx
+    dV_dx_L = hcl.compute((50, 50, 50), lambda i, j, k: spa_derivXL(i,j,k), name = "dV_dx_L")
+    dV_dx_R = hcl.compute((50, 50, 50), lambda i, j, k: spa_derivXR(i, j, k), name = "dV_dx_R")
+    dV_dx   = hcl.compute((50, 50, 50), lambda i, j, k: (dV_dx_L[i,j,k] + dV_dx_R[i,j,k])/2, name = "dV_dx")
+
+    # Calculate dV_dy
+    dV_dy_L = hcl.compute((50, 50, 50), lambda i, j, k: spa_derivYL(i, j, k), name = "dV_dy_L")
+    dV_dy_R = hcl.compute((50, 50, 50), lambda i, j, k: spa_derivYR(i, j, k), name = "dV_dy_R")
+    dV_dy   = hcl.compute((50, 50, 50), lambda i, j, k: (dV_dy_L[i,j,k] + dV_dy_R[i,j,k])/2, name = "dV_dy")
+
+    # Calculate dV_dT
+    dV_dT_L = hcl.compute((50, 50, 50), lambda i, j, k: spa_derivTL(i, j, k), name="dV_dT_L")
+    dV_dT_R = hcl.compute((50, 50, 50), lambda i, j, k: spa_derivTR(i, j, k), name="dV_dT_R")
+    dV_dT   = hcl.compute((50, 50, 50), lambda i, j, k: (dV_dy_L[i, j, k] + dV_dy_R[i, j, k]) / 2, name="dV_dT")
+
+
 
 def HJ_PDE_solver(V_new, V_init, thetas, dx):
-    hcl.config.init_dtype = hcl.Float()
 
     # HeteroC
     def my_abs(my_x):
@@ -39,7 +57,7 @@ def HJ_PDE_solver(V_new, V_init, thetas, dx):
             sign[0] = -1
         return sign[0]
 
-    # Calculate spatial derivative based on index and dimension number
+    # Calculate spatial derivative
     def spa_derivX(i,j,k):
         left_deriv = hcl.scalar(0, "left_deriv")
         right_deriv = hcl.scalar(0, "right_deriv")
@@ -56,35 +74,60 @@ def HJ_PDE_solver(V_new, V_init, thetas, dx):
         with hcl.elif_(i != 0 and i != V_init.shape[0]):
             left_deriv[0] = (V_init[i, j, k] - V_init[i - 1, j, k]) / dx[1]
             right_deriv[0] = (V_init[i + 1, j, k] - V_init[i, j, k]) / dx[1]
-
-            #return left_deriv[0],right_deriv[0]
-
-            #return left_deriv[0], right_deriv[0]
         return left_deriv[0], right_deriv[0]
 
     def spa_derivY(i,j,k):
-        return i + j +
+        left_deriv = hcl.scalar(0, "left_deriv")
+        right_deriv = hcl.scalar(0, "right_deriv")
+        with hcl.if_(i == 0):
+            left_boundary = hcl.scalar(0, "left_boundary")
+            left_boundary[0] = V_init[i, j, k] + my_abs(V_init[i + 1, j, k] - V_init[i, j, k]) * my_sign(
+                V_init[i, j, k])
+            left_deriv[0] = (V_init[i, j, k] - left_boundary[0]) / dx[1]
+            right_deriv[0] = (V_init[i + 1, j, k] - V_init[i, j, k]) / dx[1]
+        with hcl.elif_(i == V_init.shape[0]):
+            right_boundary = hcl.scalar(0, "right_boundary")
+            right_boundary[0] = V_init[i, j, k] + my_abs(V_init[i, j, k] - V_init[i - 1, j, k]) * my_sign(
+                V_init[i, j, k])
+            left_deriv[0] = (V_init[i, j, k] - V_init[i - 1, j, k]) / dx[1]
+            right_deriv[0] = (right_boundary[0] - V_init[i, j, k]) / dx[1]
+        with hcl.elif_(i != 0 and i != V_init.shape[0]):
+            left_deriv[0] = (V_init[i, j, k] - V_init[i - 1, j, k]) / dx[1]
+            right_deriv[0] = (V_init[i + 1, j, k] - V_init[i, j, k]) / dx[1]
+        return left_deriv[0], right_deriv[0]
 
-    def spa_derivTheta(i,j,k):
+
+    def spa_derivT(i,j,k):
         return i - j + k, k*j
 
     # Calculate Hamiltonian for every grid point in V_init
     with hcl.Stage("Hamiltonian"):
-        with hcl.for_(0, V_init.shape[0] + 1, name="i") as i:
+        with hcl.for_(0, V_init.shape[0] + 1, name="i") as i: # Plus 1 as for loop count stops at V_init.shape[0]
             with hcl.for_(0, V_init.shape[1] + 1, name="j") as j:
                 with hcl.for_(0, V_init.shape[2] + 1, name="k") as k:
                     # Calculate dV_dx
                     dV_dx_L = hcl.scalar(0, "dV_dx_L")
                     dV_dx_R = hcl.scalar(0, "dV_dx_R")
-                    dV_dx_C = hcl.scalar(0, "dV_dx_C")
+                    dV_dx   = hcl.scalar(0, "dV_dx")
+
+                    # Calculate dV_dy
+                    dV_dy_L = hcl.scalar(0, "dV_dy_L")
+                    dV_dy_R = hcl.scalar(0, "dV_dy_R")
+                    dV_dy   = hcl.scalar(0, "dV_dy")
+
+                    # Calculate dV_dtheta
+                    dV_dT_L = hcl.scalar(0, "dV_dT_L")
+                    dV_dT_R = hcl.scalar(0, "dV_dT_R")
+                    dV_dT   = hcl.scalar(0, "dV_dT")
+
                     dV_dx_L[0], dV_dx_R[0] = spa_derivX(i,j,k)
-                    #dV_dy_L, dV_dy_R = spa_derivY(i,j,k)
-                    #dV_dtheta_L, dV_dtheta_R = spa_derivTheta(i, j, k)
+                    dV_dy_L[0], dV_dy_R[0] = spa_derivY(i,j,k)
+                    dV_dT_L[0], dV_dT_R[0] = spa_derivTheta(i, j, k)
 
                     # Calculate average gradient
-                    dV_dx_C[0] = (dV_dx_L[0] + dV_dx_R[0])/2
-                    #dV_dy_C = (dV_dy_L + dV_dy_R) / 2
-                    #dV_dtheta_C = (dV_dtheta_L + dV_dtheta_R) / 2
+                    dV_dx[0] = (dV_dx_L + dV_dx_R) / 2
+                    dV_dy[0] = (dV_dy_L + dV_dy_R) / 2
+                    dV_dT[0] = (dV_dT_L + dV_dT_R) / 2
 
                     # Declare optimal control
                     uOpt = hcl.scalar(0, "uOpt")
@@ -96,11 +139,8 @@ def HJ_PDE_solver(V_new, V_init, thetas, dx):
                     #with hcl.if_(dV_dtheta_C > 0):
                     #    uOpt.v = -uOpt.v
 
-                    # Calculate dynamics function
-                    #V_new[i,j,k] = 1 * cos(thetas[k]) * dV_dx_C +1 * sin(thetas[k]) * dV_dy_C +uOpt * dV_theta_C
-                    #angle = hcl.scalar(thetas[k], "angle")
-                    #V_new[i,j,k] = v * hcl.cos(thetas[k]) * dV_dx_C + v * hcl.sin(thetas[k]) * dV_dy_C +  dV_dtheta_C * uOpt
-                    V_new[i, j, k] =  vel.v * hcl.cos(thetas[k]) * dV_dx_C[0]
+                    # Calculate dynamical changes
+                    V_new[i, j, k] =  vel.v * hcl.cos(thetas[k]) * dV_dx[0] + vel.v * hcl.sin(thetas[k]) * dV_dy[0] + uOpt.v * dV_dT[0]
 
 def main():
     hcl.init()
