1b3b7b568133118ab8f636786f0d5af0ca0531ca
buiminhb <buiminhb@cs-arch-22.cmpt.sfu.ca>
2020-02-01 18:01:44 -0800

    Improve speed up 20x compared to JUlia

--- HJ_PDE_HeteroCL.py
+++ HJ_PDE_HeteroCL.py
@@ -1,24 +1,55 @@
 import heterocl as hcl
 import numpy as np
+import time
 
 import math
 
 
 class grid:
-  def __init__(self, max, min, pts_each_dim, pDim):
-      self.max = max
-      self.min = min
-      self.pts_each_dim = pts_each_dim
-      self.pDim = pDim
-      self.dx = (max - min)/(pts_each_dim - 1.0)
+  def __init__(self, min, max, dims ,pts_each_dim, pDim):
+        self.max = max
+        self.min = min
+        self.dims = dims
+        self.pts_each_dim = pts_each_dim
+        self.pDim = pDim
+
+        # Make some modifications to the initialized
+        self.max[pDim] = self.min[pDim] + (self.max[pDim] - self.min[pDim])  * (1 - 1/self.pts_each_dim[pDim])
+        self.dx = (self.max - self.min) / (self.pts_each_dim - 1.0)
+        self.vs = []
+        for i in range(0,3):
+            tmp = np.arange(self.min[i], self.max[i] + self.dx[i], self.dx[i])
+            broadcast_map = np.ones(self.dims, dtype=int)
+            broadcast_map[i] = self.pts_each_dim[i]
+            tmp = np.reshape(tmp, tuple(broadcast_map))
+            self.vs.append(tmp)
+      # Reshape
+
+# This functino creates a cyclinderical shape
+def CyclinderShape(grid, ignore_dim, center, radius):
+    data = np.zeros(grid.pts_each_dim)
+
+    for i in range (0, 3):
+        if i != ignore_dim-1:
+            data = data + np.power(grid.vs[i] - center[i],  2)
+    data = np.sqrt(data) - radius
+    return data
 
 
-# NOTE: No information about structure of grid, dynamics of system passed in for now
-# Hardcoded these information
-dx = np.array([0.34, 0.22, 0.21, 0.76])
+
+
+
+g = grid(np.array([-5.0, -5.0, -math.pi]), np.array([5.0, 5.0, math.pi]), 3 ,np.array([100,100,100]), 2)
+shape = CyclinderShape(g, 3, np.zeros(3), 1)
+print(shape)
+# Declare constants
+dx = np.array([0.34, 0.22, 0.21])
 t_diff = 0.05
 
-def HJ_PDE_solver(V_new, V_init, thetas, t):
+
+
+
+def HJ_PDE_solver(V_new, V_init, thetas,t):
     # Used for calculating time bound
     #step_bound = hcl.scalar(0, "step_bound")
     #delta_T = hcl.scalar(0, "delta_T")
@@ -117,9 +148,9 @@ def HJ_PDE_solver(V_new, V_init, thetas, t):
 
     # Calculate Hamiltonian for every grid point in V_init
     with hcl.Stage("Hamiltonian"):
-        with hcl.for_(0, V_init.shape[0], name="i") as i: # Plus 1 as for loop count stops at V_init.shape[0]
+        with hcl.for_(0, V_init.shape[0], name="k") as k: # Plus 1 as for loop count stops at V_init.shape[0]
             with hcl.for_(0, V_init.shape[1], name="j") as j:
-                with hcl.for_(0, V_init.shape[2], name="k") as k:
+                with hcl.for_(0, V_init.shape[2], name="i") as i:
                     # Variables to calculate dV_dx
                     dV_dx_L = hcl.scalar(0, "dV_dx_L")
                     dV_dx_R = hcl.scalar(0, "dV_dx_R")
@@ -137,6 +168,7 @@ def HJ_PDE_solver(V_new, V_init, thetas, t):
                     dy_dt = hcl.scalar(0, "dy_dt")
                     dtheta_dt = hcl.scalar(0, "dtheta_dt")
 
+                    # No tensor slice operation
                     dV_dx_L[0], dV_dx_R[0] = spa_derivX(i, j, k)
                     dV_dy_L[0], dV_dy_R[0] = spa_derivY(i, j, k)
                     dV_dT_L[0], dV_dT_R[0] = spa_derivT(i, j, k)
@@ -195,14 +227,14 @@ def main():
     t = hcl.placeholder((1,), name="t", dtype=hcl.Float())
 
     # Create schedule
-    s = hcl.create_schedule([V_f, V_init, thetas, t], HJ_PDE_solver)
+    s = hcl.create_schedule([V_f, V_init, thetas,t], HJ_PDE_solver)
 
     # Here comes the optimization
 
     # Accessing the hamiltonian stage
     s_H = HJ_PDE_solver.Hamiltonian
     # Split the loops
-    k_out, k_in = s[s_H].split(s_H.k, 4)
+    k_out, k_in = s[s_H].split(s_H.k, 4) # These numbers are experimental, changable
     j_out, j_in = s[s_H].split(s_H.j, 4)
     i_out, i_in = s[s_H].split(s_H.i, 4)
 
@@ -211,16 +243,45 @@ def main():
     s[s_H].reorder(i_out, k_in)
     s[s_H].reorder(k_in, j_in)
 
-    # Parallel specifications - FPGA Back end
+    # FPGA Back end - parallel specs
     s[s_H].pipeline(k_in)
     s[s_H].unroll(i_out, 5)
 
     # If CPU option
-    #s[s_H].parallel(i_out)
+    s[s_H].parallel(k_out)
 
     # Inspect IR
-    print(hcl.lower(s))
+    #print(hcl.lower(s))
 
+    # Build the code - CPU Back end
+    solve_pde = hcl.build(s)
+    #print(f)
+
+    # Prepare numpy array for graph computation
+    V_0 = hcl.asarray(np.random.rand(100,100,100))
+    V_1=  hcl.asarray(np.random.rand(100,100,100))
+    t_minh = hcl.asarray(np.zeros(1))
+    list_theta = np.zeros(100)
+
+    for i in range(1,100):
+        list_theta[i] = list_theta[i-1] + dx[2]
+
+    list_theta = hcl.asarray(list_theta)
+
+
+    # Variables used for timing
+    total_time = 0
+    # Test the executable from heteroCL:
+    for i in range(0,20):
+        start = time.time()
+        solve_pde(V_1, V_0, list_theta, t_minh)
+        if i != 0: # Exclude first time of the computation
+            total_time += time.time() - start
+        print("Kernel time (s): {:.5f}".format(time.time() - start))
+        V_0 = V_1
+
+    print("Average kernel time (s): {:.5f}".format(total_time / 19))
+    print("Finished solving\n")
 
 if __name__ == '__main__':
   main()
